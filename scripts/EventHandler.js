a337b04a6cb7c1a618fbce96f31758886e2d7183
Reignard Uy <reignard.uy@dcmail.ca>
2023-01-29 12:54:11 -0500

    ICE3

--- scripts/EventHandler.js
+++ scripts/EventHandler.js
@@ -1,35 +1,32 @@
 var welcomeLabel = document.getElementById("welcome");
 var displayEvent = document.getElementById("displayEvent");
 
-var welcomeLabel = document.getElementById("userDisplay");
-var displayEvent = document.getElementById("passDisplay");
+var userDisplay = document.getElementById("userDisplay");
+var passDisplay = document.getElementById("passDisplay");
 
-welcomeLabel.addEventListener("click", function () {
-    console.log("Event: click");
-    displayEvent.textContent = "Event: clicked"
+welcomeLabel.addEventListener("dblclick", function () {
+    console.log("Event: dblclick");
+    displayEvent.textContent = "Event: dblclick";
 });
-
 welcomeLabel.addEventListener("mouseover", function () {
     console.log("Event: mouseover");
-    displayEvent.textContent = "Event: Mouse Over"
+    displayEvent.textContent = "Event: mouseover";
 });
-
 welcomeLabel.addEventListener("mouseout", function () {
     console.log("Event: mouseout");
-    displayEvent.textContent = "Event: Mouse Out"
+    displayEvent.textContent = "Event: mouseout";
 });
 
-function validateForm(){
+function validateForm() {
     let x = document.forms["myForm"]["username"].value;
     let y = document.forms["myForm"]["password"].value;
-    if(x == ""){
+    if (x == "") {
         alert("Name must be filled out");
         return false;
-    } else{
-        console.Log('Username: ${ x } \n Password: ${ y }')
+    } else {
+        console.log(`Username: ${ x } \n Password: ${ y }`)
         userDisplay.textContent = x;
-        passDIsplay.textContent = y;
+        passDisplay.textContent = y;
         return false;
     }
-
 }
